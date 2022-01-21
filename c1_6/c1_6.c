0b218ddd2a60acddb8217e8bb76af8888b6a83b0
henan-wq <nanhe31@gmail.com>
2022-01-21 11:32:24 +0800

    modified:   c1_6/c1_6.c

--- c1_6/c1_6.c
+++ c1_6/c1_6.c
@@ -23,7 +23,13 @@ main()
     }                           /* while语句指令结束        */
     printf("digits =");         /* 打印字符串digits =       */
     for (i = 0; i < 10; ++i)    /* for循环语句，第一个时初始化赋值只执行一次，第二个是循环条件，第三个是每次循环后的步长 */
-        printf(" %d", ndigit[i]);   /* 第一次执行时 */
-}
+        printf(" %d", ndigit[i]);   /* 第1次执行时i=0，打印 ndigit[0]的值，之后i=i+1，i=1 */
+                                    /* 第2次执行时i=1，打印 ndigit[1]的值，之后i=i+1，i=2 */
+                                    /* 第3次执行时i=2，打印 ndigit[2]的值，之后i=i+1，i=3 */
+                                    /* 第10次执行时i=9，打印 ndigit[1]的值，之后i=i+1，i=10 */
+                                    /* 因为i=10，所以不满足循环条件 i <10，程序向下执行 */
+    printf(", white space = %d, other = %d\n",
+        nwhite, nother);         /* 打印字符串： , white space = 变量nwhite的值, other = 变量nother的值  */
+}       /* main语句结束 */
 
-// 待完成
+// 该程序第一次出现数组，将输入的字符分成12类，用一个数组存放各个数字出现的次数，比使用10个独立的变量更方便。
