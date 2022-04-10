26c9792232e3059069c8a95d6c861dca782a9583
henan-wq <nanhe31@gmail.com>
2022-04-10 20:33:58 +0800

    modified:   README.md         new file:   file2.c         new file:   file3.c

---
+++ 6.1/video4/file3.c
@@ -0,0 +1,17 @@
+//  file3.c
+#include <stdio.h>
+
+int main()
+{
+    unsigned char c = 255;
+    int i = 255;
+    printf("c=%d, i=%d\n", c, i);
+
+    return 0;
+}
+/*
+输出结果如下：
+
+c=255, i=255
+c=255, i=255
+*/
